ff5a58ff309a05d52990ca2ad9b3a0e6a87b626b
zjwmiao <1723168479@qq.com>
2025-09-09 14:57:51 +0800

    fix：修改cookie同意的判断名称

--- javascripts/discourse/initializers/alert.js
+++ javascripts/discourse/initializers/alert.js
@@ -5,9 +5,9 @@ import { reportTagsClick } from "../lib/tags.js";
 import { reportTopicClick, reportTopicLeave } from "../lib/topic.js";
 
 function isCookieAgreed() {
-  const regexp = /\bagreed-cookiepolicy=([^;])+/;
+  const regexp = /\b_cookies_accepted=([^;]+)/;
   const res = document.cookie.match(regexp)?.[1];
-  return res === "1";
+  return res === "all" || res === "performance";
 }
 
 export default {
@@ -163,17 +163,14 @@ export default {
       }
 
       let enterTime = Date.now();
-      document.addEventListener('visibilitychange', () => {
-        if (document.visibilityState === 'visible') {
+      document.addEventListener("visibilitychange", () => {
+        if (document.visibilityState === "visible") {
           enterTime = Date.now();
         } else {
-          oaReport(
-            'leaveForum',
-            {
-              time: Date.now() - enterTime,
-              $url: location.href
-            }
-          );
+          oaReport("leaveForum", {
+            time: Date.now() - enterTime,
+            $url: location.href,
+          });
         }
       });
 
